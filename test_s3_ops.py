81cc85f46fa964166bd9bedfee76f9c67b0ae96e
hrishizone <rushikeshdongare.skn.comp@gmail.com>
2025-11-19 12:21:38 +0530

    Logging Fixed

--- test_s3_ops.py
+++ test_s3_ops.py
@@ -7,7 +7,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("logs.log"),
         logging.StreamHandler()  
     ]
 )
